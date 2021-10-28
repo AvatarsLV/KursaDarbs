da8c984eae560428f6675a9c8786c7c699de4c2b
Atvars Trams <atvars.trams@fizmati.lv>
2021-10-28 17:15:06 +0300

    4 Operations in Class1.cs

---
+++ GalaDarbs/Calculator/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Calculator
+{
+    public static class Operations
+    { 
+        public static void Sum(int firtstNum, int secondNum)
+        {
+            int result = firtstNum + secondNum;
+            Console.WriteLine($"A + B = {result}");
+        }
+        public static void Minus(int firtstNum, int secondNum)
+        {
+            int result = firtstNum - secondNum;
+            Console.WriteLine($"A - B = {result}");
+
+        }
+        public static void Times(int firtstNum, int secondNum)
+        {
+            int result = firtstNum * secondNum;
+            Console.WriteLine($"A * B = {result}");
+        }
+        public static void Divide(int firtstNum, int secondNum)
+        {
+            int result = firtstNum / secondNum;
+            Console.WriteLine($"A / B = {result}");
+
+        }
+
+        public enum MathOperations
+        {
+            Plus,
+            Minus,
+            Times,
+            Divide
+
+        }
+    }
+
+
+
+}
+
--- GalaDarbs/GalaDarbs/Program.cs
+++ GalaDarbs/GalaDarbs/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Calculator;
 
 namespace GalaDarbs
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ievadi A");
+            string aText = Console.ReadLine();
+            int aNumber = int.Parse(aText);
+            Console.WriteLine($"A ir {aNumber}");
+
+            // --------------------------------------------------------------------
+
+            Console.WriteLine("Ievadi B");
+            string bText = Console.ReadLine();
+            int bNumber = int.Parse(bText);
+
+            // --------------------------------------------------------------------
+
+            int plus = aNumber + bNumber;  //Sum(aNumber,bNumber);
+            int minus = aNumber - bNumber; //Minus(aNumber,bNumber);
+            int times = aNumber * bNumber; //Times(aNumber,bNumber);
+            int divide = aNumber / bNumber;//Divide(aNumber,bNumber);
+
+            // --------------------------------------------------------------------
+
+            Operations.Sum(aNumber, bNumber);
+            Operations.Minus(aNumber, bNumber);
+            Operations.Times(aNumber, bNumber);
+            Operations.Divide(aNumber, bNumber);
+
+
         }
+       
+   
     }
 }
