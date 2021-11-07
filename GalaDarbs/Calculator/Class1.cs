cf7d0d6cdb581481dd54e4fe390eaf723768e9a0
Atvars Trams <atvars.trams@fizmati.lv>
2021-11-07 22:08:49 +0200

    Enum, inlinesplit

--- GalaDarbs/Calculator/Class1.cs
+++ GalaDarbs/Calculator/Class1.cs
@@ -2,7 +2,7 @@
 namespace Calculator
 {
     public static class Operations
-    { 
+    {
         // TODO: 2) Failu pārsaukt Operation un arī tāpat klases nosaukumu
         // TODO: 3) uztaisīt konstruktoru, kas nolasīs ievadīto tekstu piem.:"1 + 3"
         // TODO: 4) sadala ievadīto tekstu pa atstarpēm(sākumam būs vieglāk)
@@ -12,8 +12,13 @@ namespace Calculator
         // TODO: 7) pārsaukt Calculator projektu par Calculation
         // pēc tam turpināsim tālak
 
+        //unit tests minus operacijai
+
+     
+
         public static void Sum(int firtstNum, int secondNum)
         {
+            
             int result = firtstNum + secondNum;
             Console.WriteLine($"A + B = {result}"); // returnot resultats
             //pakape logaritms
@@ -22,12 +27,13 @@ namespace Calculator
             //GD atdod calc ievadi un calc visu izdara un GD parada
             //enumi interfeisi
             //katrai darbibai ir sava klase kas atbilst vienam interfase
+            //
+            //
         }
-        public static void Minus(int firtstNum, int secondNum)
+        public static int Minus(int firtstNum, int secondNum)
         {
             int result = firtstNum - secondNum;
-            Console.WriteLine($"A - B = {result}");
-
+            return result;
         }
         public static void Times(int firtstNum, int secondNum)
         {
@@ -36,6 +42,12 @@ namespace Calculator
         }
         public static void Divide(int firtstNum, int secondNum)
         {
+            if (secondNum == 0)
+            {
+                Console.WriteLine("You cannot divide with 0");
+                return;
+            }
+
             int result = firtstNum / secondNum;
             Console.WriteLine($"A / B = {result}");
 
@@ -50,8 +62,5 @@ namespace Calculator
 
         }
     }
-
-
-
 }
 
--- GalaDarbs/GalaDarbs/Program.cs
+++ GalaDarbs/GalaDarbs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Calculator;
+using static Calculator.Operations;
 
 namespace GalaDarbs
 {
@@ -11,30 +12,73 @@ namespace GalaDarbs
             // TODO: 1) no šīs vietas tiek saukts Calculator, kas arī visu funkcionalitāti izdarīt un beigās atgriezīs ciparu
             // ieglabā mainīgajā rezultātu un to "WriteLaino"
             //piemēram: var result = Calculator.GetResult();
-            Console.WriteLine("Ievadi A"); // TODO: 0) Nav kļūda, bet taisam programmu eng valodā arī koncoles tekstu
-            string aText = Console.ReadLine();
-            int aNumber = int.Parse(aText);
-            Console.WriteLine($"A ir {aNumber}");
+
+            // DONE  TODO: 0) Nav kļūda, bet taisam programmu eng valodā arī koncoles tekstu
+            Console.WriteLine("Input equation, available operations:");
+            foreach (var opInEnum in Enum.GetValues(typeof(MathOperations)))
+            {
+                Console.WriteLine(opInEnum.ToString());
+            }
+
+            string resultEquation = Console.ReadLine();
+            string[] equationSplitet = resultEquation.Split(' ');
+            
+            string[] bText = resultEquation.Split(' ');
+            //int aNumber = int.Parse(bText[0]);
+            var aNumber = Convert.ToInt32(bText[0]);
+            var bNumber = int.Parse(bText[2]);
+            string operationSign = bText[1];
+
+
+            switch (operationSign)
+            {
+                case "+" :
+                    Operations.Sum(aNumber, bNumber);
+                    break;
+
+                case "-":
+                    Operations.Minus(aNumber, bNumber);
+                    break;
+
+                case "/":
+                    Operations.Divide(aNumber, bNumber);
+                    break;
+
+                case "*":
+                    Operations.Times(aNumber, bNumber);
+                    break;
+
+            }
+
+
+
+
+
+            //Console.WriteLine($"A is {aNumber} and B is {bNumber} ,operation is {operationSign}");
 
             // --------------------------------------------------------------------
 
-            Console.WriteLine("Ievadi B");
-            string bText = Console.ReadLine();
-            int bNumber = int.Parse(bText);
+            //int num = (int)MathOperations.Divide;
+            //Console.WriteLine($"Operation {(int)MathOperations.Divide} : {MathOperations.Divide}");
+           
 
             // --------------------------------------------------------------------
 
-            int plus = aNumber + bNumber;  //Sum(aNumber,bNumber);
-            int minus = aNumber - bNumber; //Minus(aNumber,bNumber);
-            int times = aNumber * bNumber; //Times(aNumber,bNumber);
-            int divide = aNumber / bNumber;//Divide(aNumber,bNumber);
+            //int plus = aNumber + bNumber;  //Sum(aNumber,bNumber);
+            //int minus = aNumber - bNumber; //Minus(aNumber,bNumber);
+            //int times = aNumber * bNumber; //Times(aNumber,bNumber);
+          
+
 
             // --------------------------------------------------------------------
 
-            Operations.Sum(aNumber, bNumber);
-            Operations.Minus(aNumber, bNumber);
-            Operations.Times(aNumber, bNumber);
-            Operations.Divide(aNumber, bNumber);
+            //Operations.Sum(aNumber, bNumber);
+
+            //int minusResult = Operations.Minus(aNumber, bNumber);
+            //Console.WriteLine($"A - B = {minusResult}");
+
+            //Operations.Times(aNumber, bNumber);
+            //Operations.Divide(aNumber, bNumber);
         }
     }
 }
